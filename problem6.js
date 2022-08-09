1da8337ea6eff87c3cf06bc696fd949ffc296695
hridoyroy10 <93214199+hridoyroy10@users.noreply.github.com>
2022-08-09 12:30:24 +0600

    problem6

---
+++ problem6.js
@@ -0,0 +1,14 @@
+/*
+৬. তুমি কি একটা শর্ত পালন করলে একটা কিছু করবে। শর্ত পূরণ না করলে অন্য কিছু একটা করবে এমন কোড লিখতে পারবে। অর্থাৎ তুমি কি if-else এর কোড লিখতে পারবে। পারলে একটা কোড লিখে ফেলো
+*/
+
+let r15 = 500000;
+let mayBaget = 30000;
+
+if(r15 < mayBaget){
+    console.log("R15 Baik kine futani marbo");
+}
+
+else{
+    console.log("tor taka nai bari jaye muri kha")
+}
